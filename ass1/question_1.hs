505cfc0b5ff8fe51bca3fe7d3f2d953c62dd2e56
Hector Zhao <hector.zhao@powershop.co.nz>
2017-08-06 16:16:24 +1200

    test 2-factor auth token

--- ass1/question_1.hs
+++ ass1/question_1.hs
@@ -30,8 +30,3 @@ module Question1 (
   -- helper method. should be private, if that's a thing in Haskell??
   subString :: String -> Int -> Int -> String
   subString str start size = take size $ drop start str
-
-
-
-  ----------
-  ---------
